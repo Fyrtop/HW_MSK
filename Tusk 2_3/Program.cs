500a228ca609673c3899b10581eba9fe7c91c5f9
fedorovand <fedorovand@V-71-0468>
2025-02-19 11:47:59 +0300

    Добавьте файлы проекта.

---
+++ Tusk 2_3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tusk_2_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double duim = double.Parse(Console.ReadLine());
+            double sm = duim * 2.54;
+            int m = Convert.ToInt32(sm / 100);
+            int san = Convert.ToInt32(sm % 100);
+            double mm = duim % 1;
+            Console.Write($"{duim} дюйм = {m} м {san} см {mm} мм");
+        }
+    }
+}
---
+++ Tusk_2_6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tusk_2_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double l = double.Parse(Console.ReadLine());
+            Console.WriteLine(Math.Round(l / 0.5) * 0.5);
+        }
+    }
+}
