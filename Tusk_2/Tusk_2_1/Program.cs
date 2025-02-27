5eec09f613ff70cb0f13aef55cd777e092740c46
lioun q <lioun@yandex.ru>
2025-02-27 11:44:18 +0300

    fix

---
+++ Tusk_2/Tusk_2_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tusk_2_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = 0;
+            d = c; c = b; b = a; a = d;
+            Console.Write(a + " " + b + " " + c);
+            
+
+        }
+
+    }
+}
---
+++ Tusk_2/Tusk_2_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tusk_2_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+//Угол а задан в градусах, минутах и секундах. Найти его величину в радианах.
+//Рекомендации по тестированию: проверить работоспособность программы для углов,
+//больших развернутого.
+
+            int grad = int.Parse(Console.ReadLine());
+            int minut = int.Parse(Console.ReadLine());
+            int sec = int.Parse(Console.ReadLine());
+            double ang = (grad * 3600 + minut * 60 + sec) / 3600;
+            double rad_ang = (Math.PI/180) * ang;
+            Console.WriteLine(rad_ang);
+        }
+    }
+}
