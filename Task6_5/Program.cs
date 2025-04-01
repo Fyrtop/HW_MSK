4485eae7e595fcf5a7848838141003aea5245e2a
LiNK <bbeholder@yandex.ru>
2025-04-01 11:53:07 +0300

    fix

--- Task6_5/Program.cs
+++ Task6_5/Program.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+
 
 namespace Task6_5
 {
@@ -11,10 +13,10 @@ namespace Task6_5
         static void Main(string[] args)
         {
             //Составить регулярное выражение для проверки корректности номера телефона. Корректный номер имеет формат +7(123)456-78-90.
-            string pattern = @"^\+ + [1 - 9]{1} + \( + \d{3} + \) + \d{3} + \- + \d{2} + \- + \d{2}";
+            string pattern = @"^\+[1-9]\(\d{3}\)\d{3}-\d{2}-\d{2}$";
             Regex reg = new Regex(pattern);
         string numb = Console.ReadLine();
-            Console.WriteLine(pattern.isMatch(numb) ? "OK" : "NOT OK");
+            Console.WriteLine(reg.IsMatch(numb) ? "OK" : "NOT OK");
         }
     }
 }
