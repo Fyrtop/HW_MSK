a2c2ba6798deceeca6b89c5695836a97b8c6d966
lioun q <lioun@yandex.ru>
2025-04-01 11:34:15 +0300

    fix

---
+++ Task6_4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Запросите у пользователя имя, возраст и город. Сформируйте строку в формате: "Имя: [имя], Возраст: [возраст], Город: [город]" с использованием StringBuilder.
+            StringBuilder sb = new StringBuilder();
+            Console.WriteLine("Введите имя");
+            sb.Append("Имя: ").Append(Console.ReadLine());
+            Console.WriteLine("Введите возраст");
+            sb.Append(", Возраст: ").Append(Console.ReadLine());
+            Console.WriteLine("Введите город");
+            sb.Append(", Город: ").Append(Console.ReadLine());
+            Console.Write(sb);
+
+
+        }
+    }
+}
---
+++ Task6_5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Составить регулярное выражение для проверки корректности номера телефона. Корректный номер имеет формат +7(123)456-78-90.
+            string pattern = @"^\+ + [1 - 9]{1} + \( + \d{3} + \) + \d{3} + \- + \d{2} + \- + \d{2}";
+            Regex reg = new Regex(pattern);
+        string numb = Console.ReadLine();
+            Console.WriteLine(pattern.isMatch(numb) ? "OK" : "NOT OK");
+        }
+    }
+}
